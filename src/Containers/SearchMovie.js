2558ccabd8642c5d89e0cefbbc67ca1d406651de
Y4KI <yaki07gaming@gmail.com>
2021-12-27 15:30:59 +0500

    ScrollBar changed

--- src/Containers/SearchMovie.js
+++ src/Containers/SearchMovie.js
@@ -6,17 +6,22 @@ import { SearchMovieWrap } from "../styles/SearchMovieWrap";
 function SearchMovie() {
   return (
     <SearchMovieWrap className="container">
-      <form className="d-flexs">
+      <form
+        className="d-flexs"
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
         <div className="input-group mb-3">
-          <button className="input-group-text" id="inputGroup-sizing-default">
-            <BsSearch />
-          </button>
           <input
             type="text"
             className="form-control"
             placeholder="Search for movies here!"
             onChange={(e) => searchMovie(e.target.value)}
           />
+          <button className="input-group-text" id="inputGroup-sizing-default">
+            <BsSearch />
+          </button>
         </div>
       </form>
     </SearchMovieWrap>
--- src/styles/NavbarWrap.js
+++ src/styles/NavbarWrap.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const NavbarWrap = styled.div`
   background-color: #1a374d;
-  padding: 8px 0;
+  padding: 8px 15px;
   position: sticky;
   z-index: 1000;
   top: 0;
